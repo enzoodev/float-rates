2c5a7d9e429472d2dffbf86576c688f0e1699860
Enzo Damascena <enzodm.dev@gmail.com>
2024-10-04 00:12:31 -0300

    feat: Add getRates function to fetch USD rates from API

---
+++ src/query/getRates.ts
@@ -0,0 +1,8 @@
+import { TRate } from "@/domain/entities/Rate";
+
+import { api } from "@/services/api";
+
+export const getRates = async (): Promise<Record<string, TRate>> => {
+    const response = await api.get("/usd.json");
+    return response.data;
+}
